d7c1cee21eae15d127941ac7f9123df8490c55f1
tEkaterina <ekaterina.tselishcheva@gmail.com>
2016-04-01 21:33:49 +0300

    Add contact data service.

---
+++ TourOperator/Services/Concrete/CustomerContactDataService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using DataAccess.Interfaces;
+using Models;
+using Services.Interfaces;
+
+namespace Services.Concrete
+{
+    public class CustomerContactDataService : ICustomerContactDataService
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IRepository<CustomerContactData> _contactDataRepository;
+
+        public CustomerContactDataService(IUnitOfWork uow, IRepository<CustomerContactData> contactDataRepository)
+        {
+            if(uow == null)
+                throw new ArgumentNullException(nameof(uow));
+            if(contactDataRepository == null)
+                throw new ArgumentNullException(nameof(contactDataRepository));
+
+            _uow = uow;
+            _contactDataRepository = contactDataRepository;
+        }
+
+        public void Add(CustomerContactData customerContactData)
+        {
+            if (customerContactData == null)
+                throw new ArgumentNullException(nameof(customerContactData));
+            _contactDataRepository.Create(customerContactData);
+            _uow.Commit();
+        }
+
+        public CustomerContactData GetById(int id)
+        {
+            if (id <= 0)
+                throw new ArgumentException($"Invalid specified id = {id}. Id must be greater than zero.");
+            return _contactDataRepository.GetById(id);
+        }
+    }
+}
