6035f9d0bc9312a37dc9fa8690f3ae0252f49921
tEkaterina <ekaterina.tselishcheva@gmail.com>
2016-04-01 21:36:51 +0300

    Add passport service.

---
+++ TourOperator/Services/Concrete/CustomerPassportService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using DataAccess.Interfaces;
+using Models;
+using Services.Interfaces;
+
+namespace Services.Concrete
+{
+    public class CustomerPassportService : ICustomerPassportService
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IRepository<CustomerPassport> _passportRepository;
+
+        public CustomerPassportService(IUnitOfWork uow, IRepository<CustomerPassport> passportRepository)
+        {
+            if (uow == null)
+                throw new ArgumentNullException(nameof(uow));
+            if(passportRepository == null)
+                throw new ArgumentNullException(nameof(passportRepository));
+
+            _uow = uow;
+            _passportRepository = passportRepository;
+        }
+
+        public void Add(CustomerPassport passport)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerPassport GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
